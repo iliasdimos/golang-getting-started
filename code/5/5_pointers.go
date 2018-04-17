feecc1cec7b57208fb19b40e7ac37a8f987229eb
Ilias Dimos <iliasdimos@gmail.com>
2018-04-18 00:20:25 +0300

    notes from anast

--- code/5/5_pointers.go
+++ code/5/5_pointers.go
@@ -7,6 +7,7 @@ func main() {
 	i := 42
 	p = &i
 	fmt.Println(*p) // read i through the pointer p
+	fmt.Println(p)  // value of p is a pointer
 	*p = 21         // set i through the pointer p
 	fmt.Println(i)
 }
