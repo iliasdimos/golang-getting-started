7088949a77ec128d266b9ff6297db0a7ef6d1039
Ilias Dimos <iliasdimos@gmail.com>
2018-04-23 18:40:11 +0300

    Fix embed example

--- code/6/6_embed.go
+++ code/6/6_embed.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"math"
 )
 
+// START OMIT
 type Point struct{ X, Y float64 }
 
 func (p Point) Distance(q Point) float64 {
@@ -18,7 +18,14 @@ type TextPoint struct {
 
 func main() {
 	var p = TextPoint{Point{1, 1}, "Point P"}
-	var q = TextPoint{Point{5, 4}, "Point q"}
-	fmt.Println(p.Distance(q.Point)) // "5"
-	p.Distance(q)                    // compile error: cannot use q (TextPoint) as Point
+	var q = TextPoint{Point{5, 4}, "Point Q"}
+	// Methods go with composition
+	// But a TextPoint can not be treated as a Point tin
+	d := p.Distance(q) // compile error: cannot use q (TextPoint) as Point
+
+	// Instead Get The distance from p of q.Point (Inner type)
+	dd := p.Distance(q.Point) // "5"
+
 }
+
+// END OMIT
